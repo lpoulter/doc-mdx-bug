de555d7cdf6ffa42bcfbeb3349e1177288dfaa67
laurie <laurie@brandwatch.com>
2020-01-26 17:54:58 +0000

    Update main.js config to have just one export

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,41 +1,42 @@
+const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
+
 module.exports = {
   stories: ["../src/**/*.stories.js"],
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-docs/register"
-  ]
-};
-
-const createCompiler = require("@storybook/addon-docs/mdx-compiler-plugin");
-
-module.exports = {
+  // 1. register the docs panel (as opposed to '@storybook/addon-docs' which
+  //    will configure everything with a preset)
+  addons: ['@storybook/addon-docs/register',    "@storybook/preset-create-react-app",	
+  "@storybook/addon-actions",	
+  "@storybook/addon-links"],
+  // 2. manually configure webpack, since you're not using the preset
   webpackFinal: async config => {
     config.module.rules.push({
+      // 2a. Load `.stories.mdx` / `.story.mdx` files as CSF and generate
+      //     the docs page from the markdown
       test: /\.(stories|story)\.mdx$/,
       use: [
         {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           // may or may not need this line depending on your app's setup
           options: {
-            plugins: ["@babel/plugin-transform-react-jsx"]
-          }
+            plugins: ['@babel/plugin-transform-react-jsx'],
+          },
         },
         {
-          loader: "@mdx-js/loader",
+          loader: '@mdx-js/loader',
           options: {
-            compilers: [createCompiler({})]
-          }
-        }
-      ]
+            compilers: [createCompiler({})],
+          },
+        },
+      ],
     });
+    // 2b. Run `source-loader` on story files to show their source code
+    //     automatically in `DocsPage` or the `Source` doc block.
     config.module.rules.push({
       test: /\.(stories|story)\.[tj]sx?$/,
-      loader: require.resolve("@storybook/source-loader"),
+      loader: require.resolve('@storybook/source-loader'),
       exclude: [/node_modules/],
-      enforce: "pre"
+      enforce: 'pre',
     });
     return config;
-  }
+  },
 };
